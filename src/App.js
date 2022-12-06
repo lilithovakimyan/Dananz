1d1f3cb9746697cc8bc8e42a6f0acb9d6a3e6bf2
lilithovakimyan <lilithovakimian1998@gmail.com>
2022-12-06 21:32:15 +0400

    files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import "@fontsource/poppins";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
